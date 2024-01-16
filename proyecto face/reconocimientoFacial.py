13c80ee8984286f8b68ef33e72918f269e44a85f
mapaute2 <mapaute2@utpl.edu.ec>
2024-01-16 16:18:20 -0500

    ultimos cambios

--- proyecto face/reconocimientoFacial.py
+++ proyecto face/reconocimientoFacial.py
@@ -7,12 +7,14 @@ print('imagePaths=',imagePaths)
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+# Leyendo el modelo
 face_recognizer.read('modeloLBPHFace.xml')
 
-#cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-#cap = cv2.VideoCapture('Grupal1.mp4')
-cap = cv2.VideoCapture('Grupal2.mp4')
-
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+#cap = cv2.VideoCapture('Grupal2.mp4')
+#cap = cv2.VideoCapture('Ale.mp4')
+#cap = cv2.VideoCapture('Cristhian.mp4')
+#cap = cv2.VideoCapture('Isaac.mp4')
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 while True:
@@ -29,23 +31,6 @@ while True:
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		'''
-		# EigenFaces
-		if result[1] < 5700:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
 		# LBPHFace
 		if result[1] < 90:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
